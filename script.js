f8106be145f524ce32dabcb99ae6b60366520e28
lykus <lykuskitsune@gmail.com>
2024-09-07 12:39:14 +0800

    Added UI to RPS Game

--- script.js
+++ script.js
@@ -14,63 +14,87 @@ function getComputerChoice() {
 
 // Simulates every Round
 function playRound(player, bot) {
+  let final_result = document.querySelector("#final_result");
   // Rock > Paper > Scissors > Rock...
   if (player == 'rock') {
     if (bot == 'paper') {
-      alert("Bot Wins");
+      final_result.textContent = "Bot Wins!";
       console.log("Rock < Paper");
+      return 0;
     } 
     else if (bot == 'scissors') {
-      alert("Player Wins");
+      final_result.textContent = "Player Wins!";
       console.log("Rock > Scissors");
+      return 1;
     }
     else {
-      alert("Tie");
+      final_result.textContent = "Tie!";
       console.log("Rock == Rock");
+      return 2;
     }
   }
   else if (player == 'paper') {
     if (bot == 'rock') {
-      alert("Player Wins");
+      final_result.textContent = "Player Wins!";
       console.log("Paper > Rock");
+      return 1;
     }
     else if (bot == 'scissors') {
-      alert("Bot Wins");
+      final_result.textContent = "Bot Wins!";
       console.log("Paper < Scissors");
+      return 0;
     }
     else {
-      alert("Tie");
+      final_result.textContent = "Tie!";
       console.log("Paper == Paper");
+      return 2;
     }
   }
   else {
     if (bot == 'rock') {
-      alert("Bot Wins");
+      final_result.textContent = "Bot Wins!";
       console.log("Rock > Scissors");
+      return 0;
     }
     else if (bot == 'paper') {
-      alert("Player Wins");
+      final_result.textContent = "Player Wins!";
       console.log("Paper < Scissors");
+      return 1;
     }
     else {
-      alert("Tie");
+      final_result.textContent = "Tie!";
       console.log("Scissors == Scissors");
+      return 2;
     }
   }
 }
 
 // Plays the Rounds
 function game(computerSelection, input) {
-  playRound(input, computerSelection);
-  
-  // let input = '';
-  // input = prompt("Rock, Paper, Scissors (type Cancel to Exit)");
-  // if (input.toLowerCase() == 'rock' || input.toLowerCase() == 'paper' || input.toLowerCase() == 'scissors') {
-  //   playRound(input, computerSelection);
-  // }
-  // else {
-  //   alert("Invalid or Wrong Spelling!");
-  // }
+  // For DOM Manip
+  let globalPlayerScore = document.querySelector("#player_score");
+  let globalBotScore = document.querySelector("#bot_score");
+  if(parseInt(globalBotScore.textContent) < 5 && parseInt(globalPlayerScore.textContent) < 5){
+    // Plays the Round everytime a button is clicked
+    let scoreVerdict = playRound(input, computerSelection);
+    // 1 Player win, 0 Bot Win
+    if(scoreVerdict == 0) {
+      globalBotScore.textContent = parseInt(globalBotScore.textContent) + 1;
+    }
+    else if(scoreVerdict == 1) {
+      globalPlayerScore.textContent = parseInt(globalPlayerScore.textContent) + 1;
+    }
+    else {
+      return;
+    }
+  }
 }
 
-// game(getComputerChoice());
+function reset() {
+  // For DOM Manip
+  let globalPlayerScore = document.querySelector("#player_score");
+  let globalBotScore = document.querySelector("#bot_score");
+  globalPlayerScore.textContent = '0';
+  globalBotScore.textContent = '0';
+}
+
